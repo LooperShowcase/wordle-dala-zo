eb0a9f0c48c8dfcc245e31282626dcd93bc43c86
Looper Project Showcases <87170787+LooperShowcase@users.noreply.github.com>
2022-08-02 15:38:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const server = express();
-
+const port = process.env.PORT || 3000
 const theAnswer = "bread";
 server.get("/guess/:word", (request, response) => {
   const userword = request.params.word;
@@ -20,6 +20,6 @@ server.get("/guess/:word", (request, response) => {
 
 server.use(express.static("public"));
 
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log("server is runing on port 3000");
 });

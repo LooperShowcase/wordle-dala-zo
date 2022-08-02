82d57749c85bf60e977801bd947f09ac8da6b6fd
Looper Project Showcases <87170787+LooperShowcase@users.noreply.github.com>
2022-08-02 15:32:52 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const server = express();
+
+const theAnswer = "bread";
+server.get("/guess/:word", (request, response) => {
+  const userword = request.params.word;
+  const arr = [];
+  for (let i = 0; i < userword.length; i++) {
+    const ch = userword[i];
+    if (ch == theAnswer[i]) {
+      arr.push(1);
+    } else if (theAnswer.includes(ch)) {
+      arr.push(0);
+    } else {
+      arr.push(-1);
+    }
+  }
+  response.json(arr);
+});
+
+server.use(express.static("public"));
+
+server.listen(3000, () => {
+  console.log("server is runing on port 3000");
+});
